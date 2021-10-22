eda035060f164e3f4665f0c425a7a1a3ef491e56
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-22 12:05:51 +0100

    action

---
+++ lib/action.dart
@@ -0,0 +1,9 @@
+class FetchTimeAction {
+  final String _location;
+  final String _time;
+
+  String get location => _location;
+  String get time => _time;
+
+  FetchTimeAction(this._location, this._time);
+}
