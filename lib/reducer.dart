103ce2c02333bfb4f9cc26c73b5b44e058491971
Damilare Jolayemi <djolayemi@gmail.com>
2021-10-22 12:06:58 +0100

    reducer

---
+++ lib/reducer.dart
@@ -0,0 +1,10 @@
+import 'action.dart';
+import 'app_state.dart';
+
+AppState reducer(AppState prev, dynamic action) {
+  if (action is FetchTimeAction) {
+    return AppState(action.location, action.time);
+  } else {
+    return prev;
+  }
+}
